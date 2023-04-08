dbc3d091849e5e8264339511f73d2adda9dd96ab
karenhx2 <124217201+karenhx2@users.noreply.github.com>
2023-04-08 16:06:55 -0500

    Mockups for Lessons 1-4

---
+++ CS124HonorsProjectLessons/app/src/main/java/com/example/cs124honorsprojectlessons/Playground.java
@@ -0,0 +1,8 @@
+package com.example.cs124honorsprojectlessons;
+
+public class Playground {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lesson);
+    }
+}
